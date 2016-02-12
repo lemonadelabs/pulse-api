994549a39c7b0784fb9a559c624c0180f8562f2f
will-sklenars <will.sklenars@gmail.com>
2016-02-12 15:58:10 +1300

    changed project and stakeholder resources to reference each other

--- app/resources/project_resource.rb
+++ app/resources/project_resource.rb
@@ -1,5 +1,8 @@
 class ProjectResource < JSONAPI::Resource
-  attributes :id, :name, :client, :timeframe, :timeFormat, :totalStakeholders, :projectOverview
+  attributes :id, :title, :client, :timeframe, :time_format, :total_stakeholders, :description
+
 
   has_many :stakeholder_snapshots
+  has_many :stakeholders
+
 end
--- app/resources/stakeholder_resource.rb
+++ app/resources/stakeholder_resource.rb
@@ -2,4 +2,5 @@ class StakeholderResource < JSONAPI::Resource
   attributes :id, :name, :image, :organisation, :role, :tags, :notes
 
   has_many :stakeholder_snapshots
+  has_many :projects
 end
