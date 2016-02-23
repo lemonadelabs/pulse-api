1a6786481f9f095e21157f8451d13011df281c89
will-sklenars <will.sklenars@gmail.com>
2016-02-23 14:53:12 +1300

    added in relationship routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,8 @@ Rails.application.routes.draw do
   jsonapi_resources :projects
   jsonapi_resources :stakeholders
   jsonapi_resources :stakeholder_snapshots
+  jsonapi_resources :relationships
 
   get 'projects/:project_id/stakeholders' => 'stakeholders#for_project'
 
-  # get 'projects/:project_id/stakeholder-snapshots' => 'stakeholder_snapshots#for_project'
-  # resources :stakeholder_snapshots, :path => '/stakeholder-snapshots'
-  # resources :stakeholders
-  # resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
