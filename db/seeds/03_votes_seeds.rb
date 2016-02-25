ef95dbb41820ccc00bb2428e407bc4a699a794b4
will-sklenars <will.sklenars@gmail.com>
2016-02-26 11:58:04 +1300

    vote seeds is working

--- db/seeds/03_votes_seeds.rb
+++ db/seeds/03_votes_seeds.rb
@@ -1,7 +1,20 @@
 Vote.destroy_all
 
-var stakeholder_snapshots = StakeholderSnapshot.all
+stakeholder_snapshots = StakeholderSnapshot.all
 
 stakeholder_snapshots.each do | snapshot |
-  pp snapshot
+
+  spread_power = rand(0..1.0)
+  spread_support = rand(0..1.0)
+  spread_vital = rand(0..1.0)
+
+  20.times do
+    vote = Vote.create(
+      stakeholder_snapshot: snapshot,
+      power: snapshot.power + rand(0..spread_power) - spread_power / 2,
+      support: snapshot.support + rand(0..spread_support) - spread_support / 2,
+      vital: snapshot.vital + rand(0..spread_vital) - spread_vital / 2
+    )
+    if vote.id % 10 == 0 then p "created vote #{vote.id}" ; end
+  end
 end
