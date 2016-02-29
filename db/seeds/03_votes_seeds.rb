8c78d9736bfbc97c104053aa6e2b284351d677b8
will-sklenars <will.sklenars@gmail.com>
2016-03-01 11:53:02 +1300

    changed votes seeds to avoid results that lie outside the cube

--- db/seeds/03_votes_seeds.rb
+++ db/seeds/03_votes_seeds.rb
@@ -8,13 +8,25 @@ stakeholder_snapshots.each do | snapshot |
   spread_support = rand(0..1.0)
   spread_vital = rand(0..1.0)
 
+  def normalize(float)
+    if float < 0
+      return 0
+    elsif float > 1
+      return 1
+    else
+      return float
+    end
+  end
+
   20.times do
     vote = Vote.create(
       stakeholder_snapshot: snapshot,
-      power: snapshot.power + rand(0..spread_power) - spread_power / 2,
-      support: snapshot.support + rand(0..spread_support) - spread_support / 2,
-      vital: snapshot.vital + rand(0..spread_vital) - spread_vital / 2
+      power: normalize( snapshot.power + rand(0..spread_power) - spread_power / 2),
+      support: normalize( snapshot.support + rand(0..spread_support) - spread_support / 2),
+      vital: normalize( snapshot.vital + rand(0..spread_vital) - spread_vital / 2 )
     )
     if vote.id % 10 == 0 then p "created vote #{vote.id}" ; end
   end
+
+
 end
