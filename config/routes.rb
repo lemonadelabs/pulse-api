a3dad4dd1304ca4b38f62551543e3902f3a4e0d2
will-sklenars <will.sklenars@gmail.com>
2016-03-08 16:01:20 +1300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
 
   resources :votes
   get 'projects/:project_id/connections' => 'connections#for_project'
+  get 'projects/:project_id/stakeholders/:stakeholder_id/connections' => 'connections#for_stakeholder'
 
   jsonapi_resources :projects
   jsonapi_resources :stakeholders
   jsonapi_resources :stakeholder_snapshots
 
-  get 'projects/:project_id/stakeholders/:stakeholder_id/snapshots/votes' => 'votes#for_sh_point'
+  get 'projects/:project_id/stakeholders/:stakeholder_id/votes' => 'votes#for_sh_point'
 
   get 'projects/:project_id/stakeholders' => 'stakeholders#for_project'
 
-  # get 'projects/:project_id/connections' => 'relationships#for_project'
 
 end
