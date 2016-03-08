e4476b64dc6f9dc1eca3076c80cc7724cada5cc0
will-sklenars <will.sklenars@gmail.com>
2016-03-08 16:00:39 +1300

    added for_stakeholder to connections controller

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -6,4 +6,19 @@ class ConnectionsController < ActionController::API
     render json: @connections
   end
 
+
+  def for_stakeholder
+    project = Project.find(params[:project_id])
+    stakeholder = Stakeholder.find(params[:stakeholder_id])
+    week = params[:week]
+
+    @connections = Connection.where(
+      project: project,
+      stakeholder: stakeholder,
+      week: week
+    )
+
+    render json: @connections
+  end
+
 end
