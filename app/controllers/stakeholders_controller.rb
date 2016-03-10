6e6f065508f3f9e270a8a662f0c789a155a77b98
will-sklenars <will.sklenars@gmail.com>
2016-03-11 11:34:12 +1300

    refactored route

--- app/controllers/stakeholders_controller.rb
+++ app/controllers/stakeholders_controller.rb
@@ -1,14 +1,14 @@
 class StakeholdersController < JSONAPI::ResourceController
 
   def for_project
-    project = Project.find(params[:project_id])
-    stakeholders = project.stakeholders.uniq
+    stakeholders = Project.find(params[:project_id]).stakeholders.uniq
 
     stakeholders.map! do | stakeholder |
       StakeholderResource.new(stakeholder, nil)
     end
 
     render json: JSONAPI::ResourceSerializer.new(StakeholderResource).serialize_to_hash(stakeholders)
+
   end
 
 end
