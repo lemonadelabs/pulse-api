2864cc677d454e40c16783cc92e98068a9c71c47
will-sklenars <will.sklenars@gmail.com>
2016-03-11 16:46:38 +1300

    prefix routes with `api`

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  Rails.application.routes do
+    scope(:path => '/api') do
 
-  resources :votes
-  get 'projects/:project_id/connections' => 'connections#for_project'
-  get 'projects/:project_id/stakeholders/:stakeholder_id/connections' => 'connections#for_stakeholder'
+      resources :votes
+      get 'projects/:project_id/connections' => 'connections#for_project'
+      get 'projects/:project_id/stakeholders/:stakeholder_id/connections' => 'connections#for_stakeholder'
 
-  jsonapi_resources :projects
-  jsonapi_resources :stakeholders
-  jsonapi_resources :stakeholder_snapshots
+      jsonapi_resources :projects
+      jsonapi_resources :stakeholders
+      jsonapi_resources :stakeholder_snapshots
 
-  get 'projects/:project_id/stakeholders/:stakeholder_id/votes' => 'votes#for_sh_point'
+      get 'projects/:project_id/stakeholders/:stakeholder_id/votes' => 'votes#for_sh_point'
 
-  get 'projects/:project_id/stakeholders' => 'stakeholders#for_project'
+      get 'projects/:project_id/stakeholders' => 'stakeholders#for_project'
 
-
+    end
+  end
 end
