7fb618c9f3b7683c292cd558673c150648eedb9d
will-sklenars <will.sklenars@gmail.com>
2016-03-15 10:44:31 +1300

    added module Api

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -18,5 +18,28 @@ class ConnectionsController < ActionController::API
 
     render json: @connections
   end
+end
 
+module Api
+  class ConnectionsController < ActionController::API
+    def for_project
+      project = Project.find(params[:project_id])
+      @connections = project.connections
+      render json: @connections
+    end
+
+    def for_stakeholder
+      project = Project.find(params[:project_id])
+      stakeholder = Stakeholder.find(params[:stakeholder_id])
+      week = params[:week]
+
+      @connections = Connection.where(
+        project: project,
+        stakeholder: stakeholder,
+        week: week
+      )
+
+      render json: @connections
+    end
+  end
 end
