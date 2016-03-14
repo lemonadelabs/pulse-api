6b8d3caadee95db3f7d22d4b743d8799b983be16
will-sklenars <will.sklenars@gmail.com>
2016-03-15 10:44:44 +1300

    added module Api

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,16 @@
 class VotesController < ActionController::API
-
   def for_sh_point
     @votes = Stakeholder.find(params[:stakeholder_id]).stakeholder_snapshots.find_by(project_id: params[:project_id], week: params[:week]).votes
     render json: @votes
   end
+end
 
+
+module Api
+  class VotesController < ActionController::API
+    def for_sh_point
+      @votes = Stakeholder.find(params[:stakeholder_id]).stakeholder_snapshots.find_by(project_id: params[:project_id], week: params[:week]).votes
+      render json: @votes
+    end
+  end
 end
