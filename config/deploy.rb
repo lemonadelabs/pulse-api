e518a8300d2ec21373e0dcc32546b69160e10e0d
will-sklenars <will.sklenars@gmail.com>
2016-03-15 15:52:10 +1300

    added path to default env

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,8 @@ set :linked_files, %w{config/database.yml config/secrets.yml}
 
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :default_env, { path: "/home/pulse/local/bin" } # http://stackoverflow.com/a/19735263/5522700
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
